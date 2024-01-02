2c62fdb245d6f378d39cd140e51e0055ed16d28f
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-01-01 22:38:22 -0300

    todo: detect match not found after match found

--- aceitarFila/main.py
+++ aceitarFila/main.py
@@ -7,17 +7,17 @@ while True:
   local = py.locateOnScreen('accept.png', confidence=0.6, grayscale=True)
   
   if local != None:
-    print(f'Partida encontrada. Aguardando confirmação dos outros jogadores.')
+    print(f'Partida encontrada.')
     py.click(local, duration=0.2)
-    for i in range(14):
-      print(f'Partida encontrada. Aguardando confirmação dos outros jogadores. ({i+1}/14)')
-      sleep(1)
-    local = py.locateOnScreen('accept.png', confidence=0.6, grayscale=True)
-    if local != None:
-      print(f'Partida recusada por outro jogador. Voltando para fila')
-    else:
-      print(f'Partida confirmada. Tenha um bom jogo =)')
-      break
+    # for i in range(16):
+    #   print(f'Partida encontrada. Aguardando confirmação dos outros jogadores. ({i+1}/16)')
+    #   sleep(1)
+    # local = py.locateOnScreen('accept.png', confidence=0.6, grayscale=True)
+    # if local != None:
+    #   print(f'Partida recusada por outro jogador. Voltando para fila')
+    # else:
+    #   print(f'Partida confirmada. Tenha um bom jogo =)')
+    #   break
 
   else:
     print(f'Na fila ({c})'); sleep(.5)
