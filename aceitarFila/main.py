be129572a6bb627f1f1db040ddf47c85b4e18751
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-01-05 00:38:17 -0300

    simple infinite loop

--- aceitarFila/main.py
+++ aceitarFila/main.py
@@ -9,18 +9,8 @@ while True:
   if local != None:
     print(f'Partida encontrada.')
     py.click(local, duration=0.2)
-    # for i in range(16):
-    #   print(f'Partida encontrada. Aguardando confirmação dos outros jogadores. ({i+1}/16)')
-    #   sleep(1)
-    # local = py.locateOnScreen('accept.png', confidence=0.6, grayscale=True)
-    # if local != None:
-    #   print(f'Partida recusada por outro jogador. Voltando para fila')
-    # else:
-    #   print(f'Partida confirmada. Tenha um bom jogo =)')
-    #   break
+    sleep(14); print('Voltando pra fila.'); print(1)
 
   else:
     print(f'Na fila ({c})'); sleep(.5)
     c += 1
-
-    
